241b6a58b24fc8f49689b1f0971bdc6dd561121f
Damanpreet Kaur <damanpreet1901226@gmail.com>
2026-02-03 22:13:01 -0400

    SQL complete

--- sql/churn_analysis.sql
+++ sql/churn_analysis.sql
@@ -1,6 +1,18 @@
-SELECT 
-    Contract_Month-to-month,
+SELECT
     COUNT(*) AS total_customers,
-    AVG(Churn) AS churn_rate
+    AVG(Churn * 1.0) AS churn_rate
 FROM churn_data
-GROUP BY Contract_Month-to-month;
+WHERE "Contract_One year" = 0
+  AND "Contract_Two year" = 0;
+
+SELECT
+    COUNT(*) AS total_customers,
+    AVG(Churn * 1.0) AS churn_rate
+FROM churn_data
+WHERE "Contract_One year" = 1;
+
+SELECT
+    COUNT(*) AS total_customers,
+    AVG(Churn * 1.0) AS churn_rate
+FROM churn_data
+WHERE "Contract_Two year" = 1;
