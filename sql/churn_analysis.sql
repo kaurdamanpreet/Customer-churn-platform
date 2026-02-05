68da32fec8a7ef7e49c2596a90de87b368f43342
Damanpreet Kaur <damanpreet1901226@gmail.com>
2026-02-04 21:56:31 -0400

    Create churn_summay.csv

--- sql/churn_analysis.sql
+++ sql/churn_analysis.sql
@@ -1,17 +1,25 @@
+-- Churn rate by contract type (Power BI ready)
 SELECT
+    'Month-to-month' AS contract_type,
     COUNT(*) AS total_customers,
     AVG(Churn * 1.0) AS churn_rate
 FROM churn_data
 WHERE "Contract_One year" = 0
-  AND "Contract_Two year" = 0;
+  AND "Contract_Two year" = 0
+
+UNION ALL
 
 SELECT
+    'One year' AS contract_type,
     COUNT(*) AS total_customers,
     AVG(Churn * 1.0) AS churn_rate
 FROM churn_data
-WHERE "Contract_One year" = 1;
+WHERE "Contract_One year" = 1
+
+UNION ALL
 
 SELECT
+    'Two year' AS contract_type,
     COUNT(*) AS total_customers,
     AVG(Churn * 1.0) AS churn_rate
 FROM churn_data
